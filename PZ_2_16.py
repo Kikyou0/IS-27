f52d2e6cb6e367e04159ee82e11f5ce7cddff0a8
Kikyou0 <kikyou.u0@gmail.com>
2024-10-05 15:54:43 +0300

    Add files via upload

---
+++ PZ_2_16.py
@@ -0,0 +1,13 @@
+n = int(input("Введите трехзначное число: "))
+
+suma = 0
+mult = 1
+
+while n > 0:
+    number = n % 10
+    suma = suma + number
+    mult = mult * number
+    n = n // 10
+
+print("Сумма:", suma)
+print("Произведение:", mult)
